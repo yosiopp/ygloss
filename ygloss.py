5f2f9ffe7a1e1ef736bbd9dff89bd6d79b525bb0
yosiopp <yosiopp.develop@gmail.com>
2017-03-28 18:19:27 +0900

    read frontmatter

--- ygloss.py
+++ ygloss.py
@@ -9,16 +9,33 @@ from datetime import datetime
 TMPL_INDEX='<li><a href="#%s">%s</a></li>'
 TMPL_ITEM='<dl id="%s"><dt>%s</dt><dd>%s</dd></dl>'
 
+def encode_dict(_dict, enc):
+    for k, v in _dict.items():
+        k = k.encode(enc)
+        v = v.encode(enc)
+    return _dict
+
 def replace(src, _dict):
     dest = copy.copy(src)
     for k, v in _dict.items():
         dest = dest.replace(k, v)
     return dest
 
-# TODO proc front matter
+# proc front matter
+n = 0
 lines = open(sys.argv[1], 'r').readlines()
-n = 5
-body = ''.join(lines[n:])
+if lines[0].strip() == '---':
+    for i in range(1, len(lines)):
+        if lines[i].strip() == '---':
+            n = i
+            break
+
+fm = { "lang": '', "title": '', "description": '', "date": datetime.now().isoformat()}
+try:
+    fm.update(yaml.load(''.join(lines[1:n])))
+except:
+    pass
+body = ''.join(lines[n+1:])
 gloss = yaml.load(body)
 
 items = []
@@ -35,14 +52,14 @@ for k, v in gloss.items():
     v = re.sub(r"(https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:@&=+$,%#]+)", '<a href="\\1" target="_blank">\\1</a>', v)
     items.append(TMPL_ITEM % (k, k, v))
 
-data = {
-    '${fm_lang}': 'ja', # TODO 
-    '${fm_title}': 'glossary', # TODO
-    '${fm_description}': '', # TODO
-    '${fm_date}': datetime.now().isoformat(),
-    '${indexes}': "\n".join(sorted(indexes)).encode('utf-8'),
-    '${items}': "\n".join(sorted(items)).encode('utf-8')
-}
+data = encode_dict({
+    '${fm_lang}': fm['lang'],
+    '${fm_title}': fm['title'],
+    '${fm_description}': fm['description'],
+    '${fm_date}': fm['date'],
+    '${indexes}': "\n".join(sorted(indexes)),
+    '${items}': "\n".join(sorted(items))
+}, 'utf-8')
 tmpl = open('tmpl.html').read()
 
 print replace(tmpl, data)

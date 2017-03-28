5400570b700dd073962023dc241eeec485d5dec8
yosiopp <yosiopp.develop@gmail.com>
2017-03-28 22:02:16 +0900

    refactor

--- ygloss.py
+++ ygloss.py
@@ -23,25 +23,43 @@ def replace(src, _dict):
         dest = dest.replace(k, v)
     return dest
 
+def split_frontmatter(lines):
+    n = 0
+    if lines[0].strip() != '---':
+        return ('', ''.join(lines))
+    else:
+        for i in range(1, len(lines)):
+            if lines[i].strip() == '---':
+                n = i + 1
+                break
+    return (''.join(lines[1:n-1]), ''.join(lines[n:]))
+
+def merge_dict(_base, _dict):
+    try:
+        _base.update(_dict)
+    except:
+        pass
+    return _base
+
+def curfile(file):
+    return os.path.join(os.path.dirname(__file__), file)
+
+
+## main
+
 if len(sys.argv) < 2:
     sys.stderr.write('Illegal Argument: please specify the yaml for converted.\n')
     sys.exit(1)
 
-# proc front matter
-n = 0
 lines = open(sys.argv[1], 'r').readlines()
-if lines[0].strip() == '---':
-    for i in range(1, len(lines)):
-        if lines[i].strip() == '---':
-            n = i
-            break
+frontmatter, body = split_frontmatter(lines)
 
-fm = { "lang": '', "title": '', "description": '', "date": datetime.now().isoformat()}
-try:
-    fm.update(yaml.load(''.join(lines[1:n])))
-except:
-    pass
-body = ''.join(lines[n+1:])
+fm = merge_dict({
+    'lang': 'en',
+    'title': '',
+    'description': '',
+    'date': datetime.now().isoformat()
+}, yaml.load(frontmatter))
 gloss = yaml.load(body)
 
 items = []
@@ -51,10 +69,12 @@ keywords = sorted(gloss.keys(), key=lambda x: len(x), reverse=True)
 for k, v in gloss.items():
     indexes.append(TMPL_INDEX % (k, k))
     v = v.strip().replace("\n","<br>\n")
+    # replace keywords
     for i in range(len(keywords)):
         v = v.replace(keywords[i], '{{ #KEYWORD#%d }}' % i)
     for i in range(len(keywords)):
         v = v.replace('{{ #KEYWORD#%d }}' % i, '<a href="#%s">%s</a>' % (keywords[i], keywords[i]))
+    # replace link
     v = re.sub(r"(https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:@&=+$,%#]+)", '<a href="\\1" target="_blank">\\1</a>', v)
     items.append(TMPL_ITEM % (k, k, v))
 
@@ -66,6 +86,6 @@ data = encode_dict({
     '${indexes}': "\n".join(sorted(indexes)),
     '${items}': "\n".join(sorted(items))
 }, 'utf-8')
-tmpl = open(os.path.join(os.path.dirname(__file__), TEMPLATE_FILE)).read()
+tmpl = open(curfile(TEMPLATE_FILE)).read()
 
 print replace(tmpl, data)
